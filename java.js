39032f7bdfa7d2de4c0147589ad318bef979308c
JsonSept <jasonseptember.za@gmail.com>
2023-11-24 09:57:05 +0200

    added new styling

--- java.js
+++ java.js
@@ -1,5 +1,4 @@
 document.getElementById('myHeader').innerHTML = "AREA OF A TRIANGLE CALCULATOR";
-document.getElementById('myInfo').innerHTML = "I am the boss of programming";
 document.getElementById('area').innerHTML = "Finding area of a triangle";
 
 //Function to find area of a traingle
@@ -8,5 +7,5 @@ function findAreaOfTriangle(){
   let base = document.getElementById('base').value;
  
   let areaOfTriangle = 1/2*(base * height);
-  document.getElementById('result').innerHTML = areaOfTriangle;
+  document.getElementById('result').innerText = areaOfTriangle;
 }
